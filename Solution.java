efe4bcd1b2ecd78ec4c07d3635dc8a3c0dbd0341
sdim-ua <ingersol@yandex.ua>
2019-02-03 20:45:29 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,14 @@
+/* 
+Феншуй и статики
+*/
+
+public class Solution {
+
+    public static  int A = 5;
+    public  int B = 2;
+    public int C = A * B;
+
+    public static void main(String[] args) {
+        A = 15;
+    }
+}
